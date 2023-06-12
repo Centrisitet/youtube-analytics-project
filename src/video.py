7f797d200046da5f5065b202034fdfeb7c6f4ea3
centrisitet <miklekon7@gmail.com>
2023-06-12 20:21:49 +0300

    hw_6

--- src/video.py
+++ src/video.py
@@ -8,13 +8,19 @@ class Video():
 
     def __init__(self, id_video):
         self.id = id_video
-        video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=self.id
-                                               ).execute()
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
-        self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+        try:
+            video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=self.id
+                                                   ).execute()
+            self.video_title: str = video_response['items'][0]['snippet']['title']
+            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+            self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+        except IndexError:
+            self.video_title = None
+            self.view_count = None
+            self.like_count = None
+            self.comment_count = None
 
     def __str__(self):
         return f'{self.video_title}'
